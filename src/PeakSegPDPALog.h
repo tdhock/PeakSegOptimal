6915c00f91c60f2bb42d6ef18d408b1d83f36b4d
Toby Dylan Hocking <tdhock5@gmail.com>
2016-07-12 12:52:59 -0400

    delete original positive space interval code

--- src/PeakSegPDPALog.h
+++ src/PeakSegPDPALog.h
@@ -1,12 +1,3 @@
-void PeakSegPDPA
-(int *data_vec, int *weight_vec, int data_count,
- int maxSegments,
- // the following matrices are for output, data_count x maxSegments.
- double *cost_mat,
- int *end_mat,
- double *mean_mat,
- int *intervals_mat);
-
 void PeakSegPDPALog
 (int *data_vec, int *weight_vec, int data_count,
  int maxSegments,
--- src/interface.cpp
+++ src/interface.cpp
@@ -1,6 +1,6 @@
 /* -*- compile-command: "R CMD INSTALL .." -*- */
 
-#include "PeakSegPDPA.h"
+#include "PeakSegPDPALog.h"
 
 extern "C" {
 
@@ -11,16 +11,6 @@ extern "C" {
    double *mean_mat, int *intervals_mat
    ){
     PeakSegPDPALog(data_ptr, weight_ptr, *data_count, *maxSegments,
-		   cost_mat, end_mat, mean_mat, intervals_mat);
-  }
-  
-  void PeakSegPDPA_interface
-  (int *data_ptr, int *weight_ptr,
-   int *data_count, int *maxSegments,
-   double *cost_mat, int *end_mat,
-   double *mean_mat, int *intervals_mat
-   ){
-    PeakSegPDPA(data_ptr, weight_ptr, *data_count, *maxSegments,
 		   cost_mat, end_mat, mean_mat, intervals_mat);
   }
   
