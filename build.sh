81dd858c9231502ffb6e22e850c2357ee53355cc
Toby Dylan Hocking <tdhock5@gmail.com>
2017-06-21 09:57:18 -0400

    remove all remotes when building

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd ..
 rm -rf coseg-release
 cp -r coseg coseg-release
-grep -v cosegData coseg/DESCRIPTION > coseg-release/DESCRIPTION
+grep -v cosegData coseg/DESCRIPTION | grep -v Remotes > coseg-release/DESCRIPTION
 rm coseg-release/tests/testthat/test-cosegData.R
 PKG_TGZ=$(R CMD build coseg-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
 R CMD INSTALL $PKG_TGZ
