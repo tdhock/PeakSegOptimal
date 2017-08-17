4273aab9c2bc7f11ea6b17f8d7dbefce45b819bb
Toby Dylan Hocking <tdhock5@gmail.com>
2017-08-17 12:06:17 -0400

    build script coseg -> PeakSegOptimal

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ..
-rm -rf coseg-release
-cp -r coseg coseg-release
-grep -v cosegData coseg/DESCRIPTION | grep -v Remotes > coseg-release/DESCRIPTION
-rm coseg-release/tests/testthat/test-cosegData.R
-PKG_TGZ=$(R CMD build coseg-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
+rm -rf PeakSegOptimal-release
+cp -r PeakSegOptimal PeakSegOptimal-release
+grep -v PeakSegOptimalData PeakSegOptimal/DESCRIPTION | grep -v Remotes > PeakSegOptimal-release/DESCRIPTION
+rm PeakSegOptimal-release/tests/testthat/test-PeakSegOptimalData.R
+PKG_TGZ=$(R CMD build PeakSegOptimal-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
 R CMD INSTALL $PKG_TGZ
 R CMD check --as-cran $PKG_TGZ
