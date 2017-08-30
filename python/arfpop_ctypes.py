e72b98497697e8eb3ef6889a19a33bffd2e8160c
Sean Jewell <jewellsean@gmail.com>
2017-08-30 14:24:01 -0700

    padding earlier!

--- python/arfpop_ctypes.py
+++ python/arfpop_ctypes.py
@@ -29,12 +29,11 @@ def arfpop(dat, gam, penalty, constraint):
 	out['intervals_mat'] = intervals_mat
 	out['changePts'] = np.unique(end_vec) + 1
 	out['spikes'] = out['changePts'][1:] + 1
-	out['spike_mag'] = out['mean_vec'][1:] - out['mean_vec'][0:-1] 
-
+	
 	padded = np.array([0])
 
-	out['pos_spike_mag'] = np.concatenate((padded, 
-		np.maximum(out['spike_mag'], np.zeros(out['spike_mag'].shape))))
+	out['spike_mag'] = np.concatenate((padded, out['mean_vec'][1:] - out['mean_vec'][0:-1]))
+	out['pos_spike_mag'] = np.maximum(out['spike_mag'], np.zeros(out['spike_mag'].shape))
 
 	## TODO: add error catching here! 
 
