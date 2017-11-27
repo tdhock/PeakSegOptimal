6aa98f46350340f756f22cd13c672b573e5efe27
Sean Jewell <jewellsean@gmail.com>
2017-11-27 08:03:59 -0800

    decoding slightly modified. debugging print statements

--- src/ARFPOP.cpp
+++ src/ARFPOP.cpp
@@ -5,6 +5,13 @@
 #include "funPieceListLog.h"
 #include <math.h>
 
+
+#include <stdlib.h>
+
+FILE *stream;
+
+
+
 void ARFPOP
   (double *data_vec, int data_count,
    double penalty,
@@ -22,23 +29,31 @@ void ARFPOP
    int *success){
   
   double MAX = 1e200;
-  double EPS = 1e-8;
+  double EPS = 1e-10;
+  bool pipeOut = 0;
+  
+  if (pipeOut) {
+    stream = freopen( "/Users/jewellsean/Desktop/costFunc.out", "w", stdout );
+  }
+  
   
   double min_mean=0, max_mean;
-  double scale = pow(gam, data_count + 1);
-  if (scale < INFINITY) {
-    max_mean = 0;
-    for(int data_i=0; data_i<data_count; data_i++){
-      double data = data_vec[data_i];
-      if(data > max_mean){
-        max_mean = data;
-      }
-    }
-    
-    max_mean *= (gam + 1) / scale;
-  } else {
-    max_mean = INFINITY;
-  }
+  // double scale = pow(gam, data_count + 1);
+  // if (scale < INFINITY) {
+  //   max_mean = 0;
+  //   for(int data_i=0; data_i<data_count; data_i++){
+  //     double data = data_vec[data_i];
+  //     if(data > max_mean){
+  //       max_mean = data;
+  //     }
+  //   }
+  //   
+  //   max_mean *= (gam + 1) / scale;
+  // } else {
+  //   max_mean = INFINITY;
+  // }
+  
+  max_mean = INFINITY;
   std::vector<PiecewiseSquareLoss> cost_model_mat(data_count);
   PiecewiseSquareLoss *cost, *cost_prev;
   PiecewiseSquareLoss min_prev_cost, scaled_prev_cost, min_prev_cost_scaled;
@@ -51,42 +66,71 @@ void ARFPOP
       (0.5, - data_vec[0], data_vec[0] * data_vec[0] / 2,
        min_mean, max_mean, -1, false);
       
+      // if (pipeOut) {
+      //   printf("=cost at data point i = %d \n", data_i);
+      //   cost -> print();
+      // }
+      
+
     }else{ // Alg 3 ln 6 - 8
+      
+      printf("=before-trim at data i = %d \n", data_i);
+      cost_prev -> print();
+      
       
       
       scaled_prev_cost.set_to_scaled_of(cost_prev, gam, EPS, verbose);
       
+      // printf("=after-trim at data i = %d \n", data_i);
+      // scaled_prev_cost.print();
+      
       if (*constraint) {
-        min_prev_cost.set_to_min_less_of(cost_prev, verbose);
+        min_prev_cost.set_to_min_less_of(&scaled_prev_cost, verbose);
       } else {
-        min_prev_cost.set_to_unconstrained_min_of(cost_prev, verbose);
+        min_prev_cost.set_to_unconstrained_min_of(&scaled_prev_cost, verbose);
       }
       
       min_prev_cost.set_prev_seg_end(data_i-1);
       min_prev_cost.add(0.0, 0.0, penalty);
       
-      min_prev_cost_scaled.set_to_scaled_of(&min_prev_cost, gam, EPS, verbose);
+      min_prev_cost_scaled.set_to_scaled_of(&min_prev_cost, gam, 0.0, verbose);
+      
+      // printf("=min_prev_cost_scaled at data_i__%d\n", data_i);
+      // min_prev_cost_scaled.print();
+      // printf("=scaled_prev_cost at data_i__%d\n", data_i);
+      // scaled_prev_cost.print();
+      
       
       cost->set_to_min_env_of(&min_prev_cost_scaled, &scaled_prev_cost, verbose);
+      
+      // printf("=min_min at data_i__%d\n", data_i);
+      // cost -> print();
+      
+      
+      int status = cost->check_min_of(&min_prev_cost_scaled, &scaled_prev_cost);
 
-      // int status = cost->check_min_of(&min_prev_cost_scaled, &scaled_prev_cost);
-      // 
-      // try {
-      //   if(status){
-      //     printf("Lambda = %.20e \t Gamma = %.100e\n", penalty, gam);
-      //     printf("BAD MIN ENV CHECK data_i=%d status=%d\n", data_i, status);
-      //     cost->set_to_min_env_of(&min_prev_cost_scaled, &scaled_prev_cost, false);
-      //     printf("=min_prev_cost_scaled\n");
-      //     min_prev_cost_scaled.print();
-      //     printf("=scaled_prev_cost + %f\n", penalty);
-      //     scaled_prev_cost.print();
-      //     printf("=new cost model\n");
-      //     cost->print();
-      //     throw status;
-      //   }
-      // } catch(int e) {
-      //   printf("An exception occured %d \n", e); 
+      try {
+        if(status){
+          printf("Lambda = %.20e \t Gamma = %.100e\n", penalty, gam);
+          printf("BAD MIN ENV CHECK data_i=%d status=%d\n", data_i, status);
+          cost->set_to_min_env_of(&min_prev_cost_scaled, &scaled_prev_cost, false);
+          printf("=min_prev_cost_scaled\n");
+          min_prev_cost_scaled.print();
+          printf("=scaled_prev_cost + %f\n", penalty);
+          scaled_prev_cost.print();
+          printf("=new cost model\n");
+          cost->print();
+          throw status;
+        }
+      } catch(int e) {
+        printf("An exception occured %d \n", e);
+      }
+      
+      // if (pipeOut) {
+      //   printf("=cost at data point i = %d \n", data_i);
+      //   cost -> print();
       // }
+      
       
       cost->add
         (0.5,
@@ -104,12 +148,21 @@ void ARFPOP
     
   }
   
+
+  
+  printf("All cost functions assembled\n");
+  
+  
   // Decoding the cost_model_vec, and writing to the output matrices.
   double best_cost, best_mean, prev_mean;
   int prev_seg_end=data_count;
   
   for(int i=0; i< data_count; i++){
     cost = &cost_model_mat[i];
+    
+    printf("Cost function at data_i %i\n", i);
+    cost -> print();
+    
     intervals_mat[i] = cost->piece_list.size();
     cost->Minimize
       (&best_cost, &best_mean,
@@ -119,6 +172,8 @@ void ARFPOP
     cost_mat[i] = best_cost;
   }
   
+  printf("------ start decoding -----\n");
+  
   
   // first step
   cost = &cost_model_mat[data_count - 1];
@@ -126,7 +181,6 @@ void ARFPOP
     (&best_cost, &best_mean,
      &prev_seg_end, &prev_mean);
   
-
   int prev_seg_old = data_count - 1;
   int out_i=0;
   double mean = best_mean;
@@ -135,10 +189,16 @@ void ARFPOP
   double temp_mean;
   while(prev_seg_old >= 0){
     
+    printf("next pt %i\n", prev_seg_old);
+    
     if (prev_seg_old < data_count - 1) {
       cost = &cost_model_mat[prev_seg_end];
-      cost -> findMean
-        (mean, &prev_seg_end, &prev_mean);
+      // cost -> findMean
+      //   (mean, &prev_seg_end, &prev_mean);
+      
+      cost->Minimize
+        (&best_cost, &best_mean,
+         &prev_seg_end, &prev_mean);
       
     }
     
@@ -160,4 +220,6 @@ void ARFPOP
     }
     
   }
+  
+  if (pipeOut) { fclose( stream );}
 }
