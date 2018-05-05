12dffcb3a608719d610abb1b6a56bdcdf6b3b3aa
Sean Jewell <jewellsean@gmail.com>
2018-05-04 17:33:09 -0700

    document python example

--- examples/python/simple_example.py
+++ examples/python/simple_example.py
@@ -4,6 +4,11 @@ import numpy as np
 
 gam = 0.98
 y = np.power(gam, np.concatenate([np.arange(100), np.arange(100)]))
+# Basic fit, no calcium concentration estimated
 fit = fast.arfpop(y, gam, 1, False)
 
+# Determine calcium concentration from fit
+fit = fast.fit_ar1_model(fit)
 
+# Estimate both spikes and calcium concentration
+fit = fast.arfpop(y, gam, 1, False, True)
